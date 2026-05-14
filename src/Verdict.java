ea1f2a998f15b5a4961e06b6fb92fc4368d6204f
343084331 <343084331@C-FQ6ZY72.tdsb.on.ca>
2026-05-14 10:52:32 -0400

    Filled class based on UML

--- src/Verdict.java
+++ src/Verdict.java
@@ -8,5 +8,50 @@
  * @author 343084331
  */
 public class Verdict {
+    // Attributes
+    private String caseTitle;
+    private String studentVerdict;
+    private String reason;
     
+    // Constructor
+    public Verdict(){
+        
+    }
+    
+    // Filled constructor
+    public Verdict (String caseTitle, String verdict, String reason) {
+        this.caseTitle = caseTitle;
+        this.studentVerdict = verdict;
+        this.reason = reason;
+    }
+    
+    // Setters and getters
+    public void setCaseTitle(String title) {
+        caseTitle = title;
+    }
+    
+    public String getCaseTitle() {
+        return caseTitle;
+    }
+    
+    public void setReason (String reason) {
+        this.reason = reason;
+    }
+    
+    public String getReason () {
+        return reason;
+    }
+    
+    public void setStudentVerdict(String verdict) {
+        studentVerdict = verdict;
+    }
+    
+    public String getStudentVerdict () {
+        return studentVerdict;
+    }
+    
+    // toString
+    public String toString(){
+        return "";
+    }
 }
