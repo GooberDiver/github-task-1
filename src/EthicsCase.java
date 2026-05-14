5a4449325c67cda954ebb95e92d62291c4a4d826
343084331 <343084331@C-FQ6ZY72.tdsb.on.ca>
2026-05-14 10:47:04 -0400

    Filled class based on UML

--- src/EthicsCase.java
+++ src/EthicsCase.java
@@ -8,5 +8,34 @@
  * @author 343084331
  */
 public class EthicsCase {
+    // Attributes
+    private String caseTitle;
+    private String description;
+    private String category;
+    public Verdict verdict;
     
+    // Constructor
+    public EthicsCase(String caseTitle, String description, String category) {
+        this.caseTitle = caseTitle;
+        this.description = description;
+        this.category = category;
+    }
+    
+    //Getters
+    public String getCaseTitle() {
+        return caseTitle;
+    }
+    
+    public String getCaseDescription() {
+        return description;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    // toString for output
+    public String toString() {
+        return "";
+    }
 }
